3b9cd6a79c51c3e25b35060340883fbb6a0714f9
gstragand <St33lers#>
2014-01-25 07:21:49 -0700

    Initial check-in, contains Levenshtein.

---
+++ junit/com/gms/strcomp/TestLevenshtein.java
@@ -0,0 +1,29 @@
+/*
+ * TestLevenshtein.java
+ * 
+ * Copyright 2012 George M. Stragand III.  All Rights Reserved.
+ *
+ * This software is the proprietary information of George M. Stragand III.
+ * Use is subject to license terms.
+ */
+package com.gms.strcomp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestLevenshtein
+{
+    @Test
+    public void testCompare()
+    {
+        final Levenshtein levenshtein = new Levenshtein();
+        final String str1 = "George";
+        Assert.assertEquals("Wrong", 1.0, levenshtein.compare(str1, str1), 0.0);
+        final String str2 = "Geordie";
+        final double result = levenshtein.compare(str1, str2);
+        Assert.assertTrue(0.72 > result);
+        Assert.assertTrue(0.71 < result);
+        Assert.assertEquals("Wrong", 6.0, levenshtein.compare(str1, ""), 0.0);
+        Assert.assertEquals("Wrong", 6.0, levenshtein.compare("", str1), 0.0);
+    }
+}
