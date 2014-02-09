c4849d8d9f196a19c273e72d82816b36624ee2fd
gstragand <St33lers#>
2014-02-09 15:29:33 -0700

    Initial Dice algorithm for fuzzy string match.

---
+++ src/com/gms/strcomp/Dice.java
@@ -0,0 +1,55 @@
+/*
+ * Dice.java
+ *
+ * Copyright 2014 George M. Stragand III.  All Rights Reserved.
+ *
+ * This software is the proprietary information of George M. Stragand III.
+ * Use is subject to license terms.
+ */
+package com.gms.strcomp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Dice
+{
+    public double dice(String str1, String str2)
+    {
+        return dice(bigram(str1), bigram(str2));
+
+    }
+
+    private double dice(List<char[]> bigram1, List<char[]> bigram2)
+    {
+        List<char[]> copy = new ArrayList<>(bigram2);
+        int matches = 0;
+        for (int i = bigram1.size(); --i >= 0;)
+        {
+            char[] bigram = bigram1.get(i);
+            for (int j = copy.size(); --j >= 0;)
+            {
+                char[] toMatch = copy.get(j);
+                if (bigram[0] == toMatch[0] && bigram[1] == toMatch[1])
+                {
+                    copy.remove(j);
+                    matches += 2;
+                    break;
+                }
+            }
+        }
+        return (double) matches / (bigram1.size() + bigram2.size());
+    }
+
+    private List<char[]> bigram(String input)
+    {
+        List<char[]> bigram = new ArrayList<>();
+        for (int i = 0; i < input.length() - 1; i++)
+        {
+            char[] chars = new char[2];
+            chars[0] = input.charAt(i);
+            chars[1] = input.charAt(i+1);
+            bigram.add(chars);
+        }
+        return bigram;
+    }
+}
