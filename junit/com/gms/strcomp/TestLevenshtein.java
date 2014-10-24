690eb908831985ce38c6021f21dbfb205c3c93bd
gstragand <george.stragand@gmail.com>
2014-10-24 13:50:25 -0600

    Added a new class for performing Levenshtein on names in different formats (First M Last and Last, First M) based upon the FuzzyWuzzy implementation in Python.

--- junit/com/gms/strcomp/TestLevenshtein.java
+++ junit/com/gms/strcomp/TestLevenshtein.java
@@ -26,4 +26,13 @@ public class TestLevenshtein
         Assert.assertEquals("Wrong", 6.0, levenshtein.compare(str1, ""), 0.0);
         Assert.assertEquals("Wrong", 6.0, levenshtein.compare("", str1), 0.0);
     }
+
+    @Test
+    public void testNameCompare()
+    {
+        final NameLevenshtein nameLevenshtein = new NameLevenshtein();
+        Assert.assertTrue(0.77 < nameLevenshtein.compare("George M. Stragand", "Stragand, George"));
+        Assert.assertTrue(0.76 > nameLevenshtein.compare("George M. Stragand", "King Leonidas"));
+        Assert.assertTrue(0.87 < nameLevenshtein.compare("Geordie Stragand", "George Stragand"));
+    }
 }
