b7823673e0f28dff383c490bffdc9f7fbf82e3fa
gstragand <george.stragand@gmail.com>
2014-10-24 13:54:34 -0600

    Added a new class for performing Levenshtein on names in different formats (First M Last and Last, First M) based upon the FuzzyWuzzy implementation in Python.

---
+++ src/com/gms/strcomp/NameLevenshtein.java
@@ -0,0 +1,32 @@
+/*
+ * NameLevenshtein.java
+ *
+ * Copyright 2014 George M. Stragand III.  All Rights Reserved.
+ */
+package com.gms.strcomp;
+
+import java.util.Arrays;
+
+public class NameLevenshtein
+        extends Levenshtein
+{
+    private static final String SPACE = " ";
+
+    @Override
+    public double compare(final String name1, final String name2)
+    {
+        return super.compare(tokenizeAndSort(name1), tokenizeAndSort(name2));
+    }
+
+    private static final String tokenizeAndSort(final String name)
+    {
+        String[] arrayName = name.split(" ");
+        Arrays.sort(arrayName);
+        StringBuilder nameSorted = new StringBuilder(arrayName[0]);
+        for (int i = 1; i < arrayName.length; i++)
+        {
+            nameSorted.append(SPACE).append(arrayName[i]);
+        }
+        return nameSorted.toString();
+    }
+}
